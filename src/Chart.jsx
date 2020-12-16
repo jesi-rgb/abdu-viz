82d647b4370ed675f25bbe02d2a62b574ecb24f8
Jesús Enrique Rascón <blograso@gmail.com>
2020-12-16 13:51:08 +0100

    added bubbles!

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -6,7 +6,8 @@ class BarChart extends Component {
 
     constructor(props){
         super(props);
-        this.myRef = React.createRef();
+        this.chartRef = React.createRef();
+        this.bubblesRef = React.createRef();
     }
 
     componentDidMount(){
@@ -17,7 +18,8 @@ class BarChart extends Component {
 
     render(){
         return <>
-        <div ref={this.myRef}></div>
+        <div ref={this.chartRef}></div>
+        <div ref={this.bubblesRef}></div>
         </>
     }
 
@@ -26,7 +28,16 @@ class BarChart extends Component {
         width = 460 - margin.left - margin.right,
         height = 450 - margin.top - margin.bottom;
         
-        const canvas = d3.select(this.myRef.current)
+        const barchartCanvas = d3.select(this.chartRef.current)
+                    .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform",
+                        "translate(" + margin.left + "," + margin.top + ")");
+
+
+        const bubblesCanvas = d3.select(this.bubblesRef.current)
                     .append("svg")
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", height + margin.top + margin.bottom)
@@ -36,7 +47,7 @@ class BarChart extends Component {
 
         d3.csv(`${process.env.PUBLIC_URL}/data/spoty_short.csv`, (data) => {
 
-                // GENRE CANVAS //
+                // GENRE barchartCanvas //
                 const g_groups = _.countBy(data, "genre");
 
                 const data_group = Object.keys(g_groups).map(group => {
@@ -48,14 +59,13 @@ class BarChart extends Component {
 
                 const sorted = _.sortBy(data_group, "count");
 
-
                 const xGenre = d3.scaleBand()
                     .range([0, width])
                     .domain(_.map(sorted, d => { return d.genre; }))
                     .padding(0.2);
 
 
-                canvas.append("g")
+                barchartCanvas.append("g")
                     .attr("transform", "translate(0," + height + ")")
                     .call(d3.axisBottom(xGenre))
                     .selectAll("text")
@@ -72,14 +82,14 @@ class BarChart extends Component {
                     .range(["#C3F0FF", "#9AE6FF", "#A6DAFF"]);
 
 
-                canvas.append("g")
+                barchartCanvas.append("g")
                     .attr("class", "leftAxis")
                     .call(d3.axisLeft(yGenre))
                     .style("color", "white");
 
 
 
-                canvas.selectAll("mybar")
+                barchartCanvas.selectAll("mybar")
                     .data(sorted)
                     .enter()
                     .append("rect")
@@ -89,7 +99,66 @@ class BarChart extends Component {
                     .attr("height", (d) => { return height - yGenre(d.count); })
                     .attr("fill", (d) => { return scaleColorG(d.count); });
 
+            
+                //BUBBLES
+                const props = data.map(d => {
+                    const aux = {};
+                    aux.genre = d.genre;
+                    aux.loudness = +d.loudness;
+                    aux.duration_ms = +d.duration_ms;
+                    return aux;
+                })
+            
+            
+                const xAvgDuration = d3.scaleBand()
+                                    .domain(props.map(d => d.genre))
+                                    .range([0, width])
+                                    .padding(1);
+            
+                
+                bubblesCanvas.append("g")
+                                .attr("transform", "translate(" + 0 + "," + height + ")")
+                                .call(d3.axisBottom(xAvgDuration))
+                                .selectAll("text")
+                                .attr("transform", "translate(-13,10)rotate(-90)")
+                                .style("text-anchor", "end");
+            
+                
+            
+                const yAvgDuration = d3.scaleLinear()
+                                    .domain([d3.min(props.map(d => d.loudness)), 0])
+                                    .range([height, 0]);
+            
+                bubblesCanvas.append("g")
+                                .call(d3.axisLeft(yAvgDuration));
+            
+                
+                
+                const sizeAvgDuration = d3.scaleLinear()
+                                        .domain([0, d3.max(props.map(d => d.duration_ms))])
+                                        .range([3, 20])
+            
+                const alphaScaler = d3.scaleLinear()
+                                    .domain([0, d3.max(props.map(d => d.duration_ms))])
+                                    .range([0, 0.75])
+            
+            
+                bubblesCanvas.selectAll("circles")
+                                .data(props)
+                                .enter()
+                                .append("circle")
+                                .attr("cx", (d) => {return xAvgDuration(d.genre);})
+                                .attr("cy", (d) => {return yAvgDuration(d.loudness);})
+                                .attr('r', (d) => {return sizeAvgDuration(d.duration_ms);}) // 
+                                .attr("fill", "#BC96E6")
+                                .attr('opacity', d => {return alphaScaler(d.duration_ms);})
+                            // .attr('stroke', "black")
+            
+            
             })
+
+
+            
 
     }
     
